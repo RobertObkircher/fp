7203e2ea6a16d573c5efaa0d09b01c3253abf904
Robert Obkircher <robert.obkircher@gmail.com>
2019-11-24 21:26:04 +0100

    a6

--- src/Angabe6.hs
+++ src/Angabe6.hs
@@ -1,7 +1,7 @@
 module Angabe6 where
 
-data Arith_Variable = A1 | A2 | A3 | A4 | A5 | A6 deriving (Eq,Show)
-data Log_Variable = L1 | L2 | L3 | L4 | L5 | L6 deriving (Eq,Show)
+data Arith_Variable = A1 | A2 | A3 | A4 | A5 | A6 deriving (Eq,Show,Enum)
+data Log_Variable = L1 | L2 | L3 | L4 | L5 | L6 deriving (Eq,Show,Enum)
 
 data Arith_Ausdruck
     = AK Int -- Arithmetische Konstante
@@ -26,7 +26,7 @@ data Log_Ausdruck
 
 type Arith_Variablenbelegung = Arith_Variable -> Int -- Total definierte Abb.
 type Log_Variablenbelegung = Log_Variable -> Bool -- Total definierte Abb.
-type Variablenbelegung = (Arith_Variablenbelegung,Log_Variablenbelegung)
+type Variablenbelegung = (Arith_Variablenbelegung, Log_Variablenbelegung)
 
 links :: Either a b -> a
 links (Left x) = x
@@ -52,12 +52,12 @@ instance Evaluierbar Log_Ausdruck where
 
 -- Recursively evaluates an arithmetic expression
 evaluiereArithAusdruck :: Arith_Ausdruck -> Variablenbelegung -> Int
-evaluiereArithAusdruck a vars@(avars,_) = case a of
-    AK int -> int
-    AV var -> avars var
-    Plus l r -> rec l + rec r
+evaluiereArithAusdruck a vars@(avars, _) = case a of
+    AK int    -> int
+    AV var    -> avars var
+    Plus  l r -> rec l + rec r
     Minus l r -> rec l - rec r
-    Mal l r -> rec l * rec r
+    Mal   l r -> rec l * rec r
   where
     rec :: Arith_Ausdruck -> Int
     rec = flip evaluiereArithAusdruck vars
@@ -65,12 +65,12 @@ evaluiereArithAusdruck a vars@(avars,_) = case a of
 -- Recursively evaluate a logical expression
 evaluiereLogAusdruck :: Log_Ausdruck -> Variablenbelegung -> Bool
 evaluiereLogAusdruck a vars@(_, lvars) = case a of
-    LK b -> b
-    LV var -> lvars var
-    Nicht a -> not $ rec a
-    Und l r -> rec l && rec r
-    Oder l r -> rec l || rec r
-    Gleich l r -> arith l == arith r
+    LK    b     -> b
+    LV    var   -> lvars var
+    Nicht a     -> not $ rec a
+    Und     l r -> rec l && rec r
+    Oder    l r -> rec l || rec r
+    Gleich  l r -> arith l == arith r
     Kleiner l r -> arith l < arith r
   where
     rec :: Log_Ausdruck -> Bool
@@ -91,6 +91,7 @@ data Anweisung
     | BS Log_Ausdruck Sprungadresse -- Bedingter Sprung
     | US Sprungadresse -- Unbedingter Sprung
     | MP Adresse Anweisung -- Selbstmodifikation des Programms
+    deriving (Show)
 
 type Zustand = Variablenbelegung
 type Anfangszustand = Zustand
@@ -100,14 +101,23 @@ type Programm = [Anweisung]
 type EPS = Programm
 
 --
---
+-- A1
 --
 
 interpretiere_1 :: EPS -> Anfangszustand -> Endzustand
 interpretiere_1 e = last . interpretiere_2 e
 
 interpretiere_2 :: EPS -> Anfangszustand -> [Zwischenzustand]
-interpretiere_2 eps s = map state $ run (newExecution eps s)
+interpretiere_2 eps s = map (state . fst) $ filter snd (zip execs keep)
+  where
+    execs = execute $ newExecution eps s
+    keep  = True : map isAssignment execs
+
+isAssignment :: Execution -> Bool
+isAssignment e = case instruction (pc e) of
+    Just (AZ _ _) -> True
+    Just (LZ _ _) -> True
+    _             -> False
 
 --
 --
@@ -118,57 +128,59 @@ data ProgramCounter
     | Terminated
 
 newPC :: [Anweisung] -> ProgramCounter
-newPC [] = Terminated
-newPC instructions@(x:xs) = PC [] x xs 0 (length instructions)
+newPC []                    = Terminated
+newPC instructions@(x : xs) = PC [] x xs 0 (length instructions)
 
 incPC :: ProgramCounter -> ProgramCounter
-incPC Terminated = Terminated
+incPC Terminated      = Terminated
 incPC (PC _ _ [] _ _) = Terminated
-incPC (PC left current (x:xs) n count) = PC (current:left) x xs (n + 1) count
+incPC (PC left current (x : xs) n count) =
+    PC (current : left) x xs (n + 1) count
 
 decPC :: ProgramCounter -> ProgramCounter
-decPC Terminated = Terminated
+decPC Terminated      = Terminated
 decPC (PC [] _ _ _ _) = Terminated
-decPC (PC (x:xs) current right index count) = PC xs x (current:right) (index - 1) count
+decPC (PC (x : xs) current right index count) =
+    PC xs x (current : right) (index - 1) count
 
 goto :: Int -> ProgramCounter -> ProgramCounter
-goto n pc@(PC _ _ _ index count)
-  | n >= count = Terminated
-  | n < 0 = Terminated
-  | n > index = incPC pc
-  | n < index = decPC pc
+goto n pc@(PC _ _ _ index count) | n >= count = Terminated
+                                 | n < 0      = Terminated
+                                 | n > index  = goto n $ incPC pc
+                                 | n < index  = goto n $ decPC pc
 goto _ pc = pc
 
 gotoOrAppend :: Int -> ProgramCounter -> ProgramCounter
 gotoOrAppend n pc@(PC left current right index count)
-  | n /= count = goto n pc
-  | index /= lastIndex = gotoOrAppend n $ goto lastIndex pc
-  | index == lastIndex = PC (current:left) (US 42) [] n n
+    | n /= count         = goto n pc
+    | index /= lastIndex = gotoOrAppend n $ goto lastIndex pc
+    | index == lastIndex = PC (current : left) (US 42) [] n n
   where
     lastIndex :: Adresse
     lastIndex = count - 1
 
 instruction :: ProgramCounter -> Maybe Anweisung
-instruction Terminated = Nothing
+instruction Terminated           = Nothing
 instruction (PC _ current _ _ _) = Just current
 
 setInstruction :: Anweisung -> ProgramCounter -> ProgramCounter
 setInstruction _ Terminated = Terminated
-setInstruction instr (PC left _ right index count) = PC left instr right index count
+setInstruction instr (PC left _ right index count) =
+    PC left instr right index count
 
 --
 --
 --
 
-data Execution = Execution 
+data Execution = Execution
   { pc :: ProgramCounter
   , state :: Zustand
   }
 
 running :: Execution -> Bool
-running e = case pc e of 
-  Terminated -> False
-  _ -> True
+running e = case pc e of
+    Terminated -> False
+    _          -> True
 
 newExecution :: [Anweisung] -> Anfangszustand -> Execution
 newExecution = Execution . newPC
@@ -179,8 +191,11 @@ modifyState f e = e { state = f (state e) }
 modifyPC :: (ProgramCounter -> ProgramCounter) -> Execution -> Execution
 modifyPC f e = e { pc = f (pc e) }
 
-run :: Execution -> [Execution]
-run = takeWhile running . iterate step
+execute :: Execution -> [Execution]
+execute = takeWhile' running . iterate step
+
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' p = foldr (\x xs -> x : if p x then xs else []) []
 
 --
 --
@@ -188,26 +203,109 @@ run = takeWhile running . iterate step
 
 step :: Execution -> Execution
 step e = case instruction (pc e) of
-  Nothing -> e
-  Just instr -> case instr of
-    AZ arithVar arithExpr -> modifyPC incPC $ modifyState (setArithVariable arithVar arithExpr) e
-    LZ logVar logExpr -> modifyPC incPC $ modifyState (setLogVariable logVar logExpr) e
-    FU logExpr addrL addrR -> if evaluiereLogAusdruck logExpr (state e) then modifyPC (goto addrL) e else modifyPC (goto addrR) e
-    BS logExpr addr -> if evaluiereLogAusdruck logExpr (state e) then modifyPC (goto addr) e else modifyPC incPC e
-    US addr -> modifyPC (goto addr) e
-    MP addr instr -> modifyPC (setInstruction instr . gotoOrAppend addr) e
+    Nothing    -> e
+    Just instr -> case instr of
+        AZ arithVar arithExpr ->
+            modifyPC incPC $ modifyState (setArithVariable arithVar arithExpr) e
+        LZ logVar logExpr ->
+            modifyPC incPC $ modifyState (setLogVariable logVar logExpr) e
+        FU logExpr addrL addrR -> modifyPC
+            (if evaluiereLogAusdruck logExpr (state e)
+                then goto addrL
+                else goto addrR
+            )
+            e
+        BS logExpr addr -> modifyPC
+            (if evaluiereLogAusdruck logExpr (state e) then goto addr else incPC
+            )
+            e
+        US addr       -> modifyPC (goto addr) e
+        MP addr instr -> modifyPC (setInstruction instr . gotoOrAppend addr) e
 
-
-setArithVariable :: Arith_Variable -> Arith_Ausdruck -> Variablenbelegung -> Variablenbelegung
+setArithVariable
+    :: Arith_Variable
+    -> Arith_Ausdruck
+    -> Variablenbelegung
+    -> Variablenbelegung
 setArithVariable var value s@(arithVars, logVars) = (arithVars', logVars)
   where
     arithVars' :: Arith_Variable -> Int
-    arithVars' v = if v == var then evaluiereArithAusdruck value s else arithVars v
+    arithVars' v =
+        if v == var then evaluiereArithAusdruck value s else arithVars v
 
-setLogVariable :: Log_Variable -> Log_Ausdruck -> Variablenbelegung -> Variablenbelegung
+setLogVariable
+    :: Log_Variable -> Log_Ausdruck -> Variablenbelegung -> Variablenbelegung
 setLogVariable var value s@(arithVars, logVars) = (arithVars, logVars')
   where
     logVars' :: Log_Variable -> Bool
     logVars' v = if v == var then evaluiereLogAusdruck value s else logVars v
 
+--
+-- A2
+--
 
+gib_aus_arith_Varbel :: Arith_Variablenbelegung -> [(Arith_Variable, Int)]
+gib_aus_arith_Varbel v = map (\l -> (l, v l)) [A1 .. A6]
+
+gib_aus_log_Varbel :: Log_Variablenbelegung -> [(Log_Variable, Bool)]
+gib_aus_log_Varbel v = map (\l -> (l, v l)) [L1 .. L6]
+
+gib_aus_Zustand :: Zustand -> ([(Arith_Variable, Int)], [(Log_Variable, Bool)])
+gib_aus_Zustand (a, l) = (gib_aus_arith_Varbel a, gib_aus_log_Varbel l)
+
+
+--
+-- A3
+--
+
+
+ggt :: EPS
+ggt =
+    [ BS (Gleich a1 a2)          6
+    , BS (Nicht (Kleiner a2 a1)) 4
+    , AZ A1 $ Minus a1 a2
+    , US 0
+    , AZ A2 $ Minus a2 a1
+    , US 0
+    , AZ A3 a1
+    ]
+  where
+    a1 = AV A1
+    a2 = AV A2
+
+fibo :: EPS
+fibo =
+    [ AZ A6 k0
+    , AZ A5 $ AK 1
+    , LZ L1 $ Nicht $ Kleiner a1 k0
+    , BS l1 loop
+    , AZ A1 (Minus k0 a1)
+    , BS (Gleich a1 k0) (length fibo - 2) -- loop
+    , AZ A4 (Plus a6 a5)
+    , AZ A6 a5
+    , AZ A5 a4
+    , US loop
+    , BS l1 (length fibo)
+    , AZ A6 (Minus k0 a6)
+    ]
+  where
+    l1   = LV L1 -- not negative
+    a1   = AV A1 -- loop counter
+    a4   = AV A4 -- fib(n+2) tmp
+    a5   = AV A5 -- fib(n+1)
+    a6   = AV A6 -- fib(n)
+    k0   = AK 0
+    loop = 5
+
+
+azst1 :: Anfangszustand
+azst1 = (enumIndex ([24,60] ++ repeat 0), const True)
+
+azst2 :: Anfangszustand
+azst2 = (enumIndex ([18, 45] ++ [3..]), enumIndex (cycle [False, True]))
+
+generiere :: [Int] -> [Bool] -> Zustand
+generiere is bs = (enumIndex (is ++ repeat 0), enumIndex (bs ++ repeat False))
+
+enumIndex :: Enum i => [a] -> i -> a
+enumIndex xs i = xs !! fromEnum i
--- src/Angabe6Test.hs
+++ src/Angabe6Test.hs
@@ -1,43 +1,115 @@
 module Angabe6Test
-    ( module Test
+    ( module Test.HUnit
     , module Angabe6
     , module Angabe6Test
-    ) where
+    )
+where
 
-import Test
-import Angabe6
-
-test_all = do
-    test
+import           Test.HUnit
+import           Angabe6
 
 --
---
+-- All tests
 --
 
-fib n
-  | n == 0 = 0
-  | n == 1 = 1
-  | n > 0 = fib (n-2) + fib (n-1)
-  | True = n
+testAll :: IO ()
+testAll = do
+    x <- runTestTT testAngabe6
+    return ()
 
-fac n
-  | n == 0 = 1
-  | n > 0 = n * fac (n-1)
-  | True = n
+testAngabe6 :: Test
+testAngabe6 = TestList
+    [ testPi0zst0Informell
+    , testPi0zst0
+    , testPi1
+    , testPi2
+    , testPi4
+    , testPi5
+    , testA2
+    , testA3ggt
+    ]
+
+testNontermination1 = gib_aus_Zustand (interpretiere_1 pi3 zst1)
+
+testNontermination2 = do
+    putStr "Expected: "
+    print $ map gib_aus_Zustand [zst1]
+    putStr "Actual: "
+    print $ map gib_aus_Zustand (take 100 (interpretiere_2 pi3 zst1))
+
+--
+-- printf debugging is fun
+--
+
+showPC :: ProgramCounter -> String
+showPC pc = case pc of
+    Terminated -> "Terminated"
+    (PC left current right index count) ->
+        "(PC "
+            ++ show index
+            ++ " "
+            ++ show count
+            ++ ":"
+            ++ concatMap showA (reverse left)
+            ++ '❤'
+            :  show current
+            ++ "❤"
+            ++ concatMap showA right
+            ++ ")"
+        where showA a = ' ' : show a ++ " "
+
+showState :: Zustand -> String
+showState (a, l) =
+    "(A"
+        ++ concatMap f [A1 .. A6]
+        ++ ")"
+        ++ "(L"
+        ++ concatMap g [L1 .. L6]
+        ++ ")"
+  where
+    f x = " " ++ show (a x)
+    g x = " " ++ if l x then "T" else "F"
+
+showExecution :: Execution -> String
+showExecution e = showState (state e) ++ " " ++ showPC (pc e)
+
+
+printExecution :: [Anweisung] -> Anfangszustand -> IO ()
+printExecution prog state = do
+    let execs = take 100 $ execute (newExecution prog state)
+    putStrLn (unlines (map showExecution execs))
+
+--
+-- Tests for A1
+--
+
+fib n | n == 0    = 0
+      | n == 1    = 1
+      | n > 0     = fib (n - 2) + fib (n - 1)
+      | otherwise = n
+
+fac n | n == 0    = 1
+      | n > 0     = n * fac (n - 1)
+      | otherwise = n
 
 -- Programm pi0: Berechnung der Fakultaet fuer den Anfangswert von A1, falls
 -- dieser groesser oder gleich 0 ist, in A2:
-pi0 = [AZ A2 (AK 1),BS (Gleich (AV A1) (AK 1)) 999, AZ A2 (Mal (AV A2) (AV A1)),AZ A1 (Minus (AV A1) (AK 1)),US 1]
-
+pi0 =
+    [ AZ A2 (AK 1)
+    , BS (Gleich (AV A1) (AK 1)) 999
+    , AZ A2 (Mal (AV A2) (AV A1))
+    , AZ A1 (Minus (AV A1) (AK 1))
+    , US 1
+    ]
 
 lvb0 :: Log_Variablenbelegung
-lvb0 lv =  True
+lvb0 lv = True
 
 avb0 :: Arith_Variablenbelegung
-avb0 av =  if av == A1 then fib 4 else fib 6 - (fac 3 + 2)
+avb0 av = if av == A1 then fib 4 else fib 6 - (fac 3 + 2)
 
 zst0 :: Anfangszustand
-zst0 = (avb0,lvb0)
+zst0 = (avb0, lvb0)
 
 avb1 :: Arith_Variablenbelegung
 avb1 av = if av == A2 then 1 else avb0 av
@@ -47,110 +119,296 @@ avb2 av = if av == A2 then 3 else avb0 av
 
 avb3 :: Arith_Variablenbelegung
 avb3 av = case av of
-  A1 -> 2
-  A2 -> 3
-  x -> avb0 x
+    A1 -> 2
+    A2 -> 3
+    x  -> avb0 x
 
 avb4 :: Arith_Variablenbelegung
 avb4 av = case av of
-  A1 -> 2
-  A2 -> 6
-  x -> avb0 x
+    A1 -> 2
+    A2 -> 6
+    x  -> avb0 x
 
 avb5 :: Arith_Variablenbelegung
 avb5 av = case av of
-  A1 -> 1
-  A2 -> 6
-  x -> avb0 x
+    A1 -> 1
+    A2 -> 6
+    x  -> avb0 x
 
-{-
+-- Informell, keine direkte Ausgabe am Bildschirm moeglich (s.a. A.2).
+testPi0zst0Informell :: Test
+testPi0zst0Informell = "testPi0zst0Informell" ~: test
+    [ gib_aus_Zustand (interpretiere_1 pi0 zst0)
+        @?= gib_aus_Zustand (avb5, lvb0)
+    , map gib_aus_Zustand (interpretiere_2 pi0 zst0) @?= map
+        gib_aus_Zustand
+        [ (avb0, lvb0)
+        , (avb1, lvb0)
+        , (avb2, lvb0)
+        , (avb3, lvb0)
+        , (avb4, lvb0)
+        , (avb5, lvb0)
+        ]
+    , map gib_aus_Zustand (take 2 (drop 3 (interpretiere_2 pi0 zst0)))
+        @?= map gib_aus_Zustand [(avb3, lvb0), (avb4, lvb0)]
+    ]
 
-interpretiere_1 pi0 zst0 ->> (avb5,lvb0) -- Informell, keine direkte Ausgabe am Bildschirm moeglich (s.a. A.2).
-interpretiere_2 pi0 zst0 ->> [(avb0,lvb0),(avb1,lvb0),(avb2,lvb0), (avb3,lvb0),(avb4,lvb0),(avb5,lvb0)] -- s.o.
-take 2 (drop 3 (interpretiere_2 pi0 zst0)) ->> [(avb3,lvb0),(avb4,lvb0)] -- s.o.
 
 -- Unmittelbare Bildschirmausgaben moeglich z.B. fuer:
-fst (interpretiere_1 pi0 zst0) A1 ->> fst (avb5,lv0) A1 ->> avb5 A1 ->> 1
-fst (interpretiere_1 pi0 zst0) A2 ->> fst (avb5,lv0) A2 ->> avb5 A2 ->> 6
-fst (interpretiere_1 pi0 zst0) A3 ->> fst (avb5,lv0) A3 ->> avb5 A3 ->> 0
-fst (interpretiere_1 pi0 zst0) A4 ->> fst (avb5,lv0) A4 ->> avb5 A4 ->> 0
-fst (interpretiere_1 pi0 zst0) A5 ->> fst (avb5,lv0) A5 ->> avb5 A5 ->> 0
-fst (interpretiere_1 pi0 zst0) A6 ->> fst (avb5,lv0) A6 ->> avb5 A6 ->> 0
+testPi0zst0 :: Test
+testPi0zst0 = "pi0 zst0" ~: test
+    [ fst (interpretiere_1 pi0 zst0) A1 @?= 1
+    , fst (interpretiere_1 pi0 zst0) A2 @?= 6
+    , fst (interpretiere_1 pi0 zst0) A3 @?= 0
+    , fst (interpretiere_1 pi0 zst0) A4 @?= 0
+    , fst (interpretiere_1 pi0 zst0) A5 @?= 0
+    , fst (interpretiere_1 pi0 zst0) A6 @?= 0
+    , fst (avb5, lvb0) A1 @?= 1
+    , fst (avb5, lvb0) A2 @?= 6
+    , fst (avb5, lvb0) A3 @?= 0
+    , fst (avb5, lvb0) A4 @?= 0
+    , fst (avb5, lvb0) A5 @?= 0
+    , fst (avb5, lvb0) A6 @?= 0
+    , avb5 A1 @?= 1
+    , avb5 A2 @?= 6
+    , avb5 A3 @?= 0
+    , avb5 A4 @?= 0
+    , avb5 A5 @?= 0
+    , avb5 A6 @?= 0
+  --
+    , fst (interpretiere_2 pi0 zst0 !! 0) A1 @?= 3
+    , fst (interpretiere_2 pi0 zst0 !! 1) A6 @?= 0
+    , fst (interpretiere_2 pi0 zst0 !! 2) A1 @?= 3
+    , fst (interpretiere_2 pi0 zst0 !! 3) A2 @?= 3
+    , fst (interpretiere_2 pi0 zst0 !! 4) A1 @?= 2
+    , fst (interpretiere_2 pi0 zst0 !! 5) A1 @?= 1
+    , fst (interpretiere_2 pi0 zst0 !! 5) A2 @?= 6
+    , fst (interpretiere_2 pi0 zst0 !! 5) A3 @?= 0
+    , avb0 A1 @?= 3
+    , avb1 A6 @?= 0
+    , avb2 A1 @?= 3
+    , avb3 A2 @?= 3
+    , avb4 A1 @?= 2
+    , avb5 A1 @?= 1
+    , avb5 A2 @?= 6
+    , avb5 A3 @?= 0
+    ]
 
-fst ((interpretiere_2 pi0 zst0) !!  0) A1 ->> ...  ->> avb0 A1 ->> 3
-fst ((interpretiere_2 pi0 zst0) !!  1) A6 ->> ...  ->> avb1 A6 ->> 0
-fst ((interpretiere_2 pi0 zst0) !!  2) A1 ->> ...  ->> avb2 A1 ->> 3
-fst ((interpretiere_2 pi0 zst0) !!  3) A2 ->> ...  ->> avb3 A2 ->> 3
-fst ((interpretiere_2 pi0 zst0) !!  4) A1 ->> ...  ->> avb4 A1 ->> 2
-fst ((interpretiere_2 pi0 zst0) !!  5) A1 ->> ...  ->> avb5 A1 ->> 1
-fst ((interpretiere_2 pi0 zst0) !!  5) A2 ->> ...  ->> avb5 A2 ->> 6
-fst ((interpretiere_2 pi0 zst0) !!  5) A3 ->> ...  ->> avb5 A3 ->> 0
 
 -- Programm pi1: Leeres Programm
 pi1 = []
-interpretiere_1 pi1 zst0 ->> zst0 -- s.o.
-interpretiere_2 pi1 zst0 ->> [zst0] -- s.o.
+
+testPi1 :: Test
+testPi1 = "pi1" ~: test
+    [ gib_aus_Zustand (interpretiere_1 pi1 zst0) @?= gib_aus_Zustand zst0
+    , map gib_aus_Zustand (interpretiere_2 pi1 zst0)
+        @?= map gib_aus_Zustand [zst0]
+    ]
+
 
 -- Programm pi2: Selbstmodifizierendes Programm
-pi2 = [MP ((fib (fac 3)) - (fac (1+2) + 2)) (AZ A3 (AK (5*(fib (fac 3))+2)))]
+pi2 =
+    [ MP ((fib (fac 3)) - (fac (1 + 2) + 2))
+         (AZ A3 (AK (5 * (fib (fac 3)) + 2)))
+    ]
 
-avb6 = \ av -> 1 :: Arith_Variablenbelegung
-zst1 = (avb6,lvb0) :: Anfangszustand
-zst2 = (\ av -> if av == A3 then (fac 3) * (fib 7 - fac 3) else avb6 av,
-        \ lv -> if lv /= L6
-                    then (True /= False) && True
-                    else (mod (4 * (avb6 A3)) (2 + fac (avb6 A1)) /= 0) )
-       :: Anfangszustand
+avb6 :: Arith_Variablenbelegung
+avb6 av = 1
 
-interpretiere_1 pi2 zst1 ->> (\ av -> if av == A3 then 42 else fst zst1 av,snd zst1) -- s.o.
+zst1 :: Anfangszustand
+zst1 = (avb6, lvb0)
 
-interpretiere_2 pi2 zst1 ->> [zst1,(\ av -> if av == A3 then 42 else fst zst1 av,snd zst1)] -- s.o.
-interpretiere_1 pi2 zst2 ‘==’ interpretiere_1 pi2 zst1 -- symbolisch: (Listen
-interpretiere_2 pi2 zst2 ‘==’ interpretiere_2 pi2 zst1 -- von) Fkt. nicht in Eq
+zst2 :: Anfangszustand
+zst2 =
+    ( \av -> if av == A3 then fac 3 * (fib 7 - fac 3) else avb6 av
+    , \lv -> (lv /= L6) || (mod (4 * avb6 A3) (2 + fac (avb6 A1)) /= 0)
+    )
+
+
+testPi2 :: Test
+testPi2 = "pi2" ~: test
+    [ gib_aus_Zustand (interpretiere_1 pi2 zst1) @?= gib_aus_Zustand
+        (\av -> if av == A3 then 42 else fst zst1 av, snd zst1)
+    , map gib_aus_Zustand (interpretiere_2 pi2 zst1) @?= map
+        gib_aus_Zustand
+        [zst1, (\av -> if av == A3 then 42 else fst zst1 av, snd zst1)]
+    , gib_aus_Zustand (interpretiere_1 pi2 zst2)
+        @?= gib_aus_Zustand (interpretiere_1 pi2 zst1)
+    --, map gib_aus_Zustand (interpretiere_2 pi2 zst2) @?= map gib_aus_Zustand (interpretiere_2 pi2 zst1) --Der erste Zustand in dieser Liste (d.h. an Indexposition 0) ist der Anfangszustand σ
+    ]
 
 
 -- Programm pi3: Selbstmodifizierendes, nichtterminierendes Programm
-pi3 = [MP ((fac 3 + 2) - (fib (fac 3)))
-            (BS (LK ((fac 5) > (fib 5))) (fib (fac 3) - (fac 3 + 2)))]
-        ++ pi0 ++ pi2
+pi3 =
+    [ MP ((fac 3 + 2) - (fib (fac 3)))
+         (BS (LK ((fac 5) > (fib 5))) (fib (fac 3) - (fac 3 + 2)))
+        ]
+        ++ pi0
+        ++ pi2
 
-interpretiere_1 pi3 zst1 ->> ‘undefiniert wg. Nichtterminierung’
-interpretiere_2 pi3 zst1 ->> [zst1.. -- terminiert nicht, aber fuehrt keine
-                                     -- zustandsveraendernde Anweisung aus;
-                                     -- deshalb erscheint nur das erste Listen-
-                                     -- element gefolgt von unendlichem Warten.
+{-
+testPi3 :: Test
+testPi3 = "testPi3" ~: test
+    [ gib_aus_Zustand (interpretiere_1 pi3 zst1) @?= error "Should not terminate"
+    , map gib_aus_Zustand (take 100 (interpretiere_2 pi3 zst1)) @?= map gib_aus_Zustand [zst1]
+    ]
+    -}
 
-drop 2 (interpretiere_2 pi3 zst1) ->> ...
+{-
+drop 2 (interpretiere_2 pi3 zst1) @?= ...
 -- unendliches Warten, da interpretiere_2 nicht terminiert,
 -- aber nach dem ersten keine weiteren Listenelemente liefert.
+-}
 
 -- Programm pi4: Nichtterminierendes, kontinuierlich zustands-
 -- veraenderndes Programm
 pi4 = [LZ L1 (Nicht (LV L1)), BS (LK True) 0]
-interpretiere_1 pi4 zst1 ->> ‘undefiniert wg. Nichtterminierung’
-interpretiere_2 pi4 zst1 ->> [zst1,zst2,zst1,zst2,zst1,zst2,..
+--interpretiere_1 pi4 zst1 @?= ‘undefiniert wg. Nichtterminierung’
+--interpretiere_2 pi4 zst1 @?= [zst1,zst2,zst1,zst2,zst1,zst2,..
 -- terminiert nicht regulaer, sondern irregulaer, wenn der gesamte
 -- Maschinenspeicher aufgebraucht ist
-    wobei zst2 = (fst zst1,\ lv -> if lv == L1 then False else True)
+--    wobei zst2 = (fst zst1,\ lv -> if lv == L1 then False else True)
 
-take 4 (interpretiere_2 pi4 zst1) ->> [zst1,zst2,zst1,zst2] -- nicht unmittelbar ausgebbar, s.o.
-fst (take 4 (interpretiere_2 pi4 zst1) !! 3) A3 ->> ... ->> 1
-snd (take 4 (interpretiere_2 pi4 zst1) !! 1) L1 ->> ... ->> False
-snd (take 4 (interpretiere_2 pi4 zst1) !! 1) L2 ->> ... ->> True
+testPi4 :: Test
+testPi4 = "testPi4" ~: test
+    [ -- map gib_aus_Zustand (take 4 (interpretiere_2 pi4 zst1)) @?= map gib_aus_Zustand [zst1, zst2, zst1, zst2] -- ????????????
+      fst (take 4 (interpretiere_2 pi4 zst1) !! 3) A3 @?= 1
+    , snd (take 4 (interpretiere_2 pi4 zst1) !! 1) L1 @?= False
+    , snd (take 4 (interpretiere_2 pi4 zst1) !! 1) L2 @?= True
+    ]
 
 -- Programm pi5: Nichtterminierendes, formal (nicht tatsaechlich) zustands-
 -- veraenderndes Programm (der Zustand wird ident ueberschrieben)
 pi5 = [LZ L1 (LV L1), BS (LK True) 0]
-interpretiere_1 pi5 zst1 ->> ‘undefiniert wg. Nichtterminierung’
-interpretiere_2 pi5 zst1 ->> [zst1,zst1,zst1,zst1,zst1,..
+--interpretiere_1 pi5 zst1 @?= ‘undefiniert wg. Nichtterminierung’
+--interpretiere_2 pi5 zst1 @?= [zst1,zst1,zst1,zst1,zst1,..
 -- terminiert nicht regulaer, sondern irregulaer, wenn der gesamte
 -- Maschinenspeicher aufgebraucht ist
-    wobei zst2 = (fst zst1,\ lv -> if lv == L1 then False else True)
+--    wobei zst2 = (fst zst1,\ lv -> if lv == L1 then False else True)
 
-take 5 (interpretiere_2 pi5 zst1) ->> [zst1,zst1,zst1,zst1,zst1] -- nicht unmittelbar ausgebbar, s.o.
-fst (take 5 (interpretiere_2 pi5 zst1) !! 3) A3 ->> ... ->> 1
-snd (take 4 (interpretiere_2 pi5 zst1) !! 1) L1 ->> ... ->> True
-snd (take 4 (interpretiere_2 pi5 zst1) !! 1) L2 ->> ... ->> True
-snd (take 5 (interpretiere_2 pi5 zst1) !! 2) L5 ->> ... ->> True
+testPi5 :: Test
+testPi5 = "testPi5" ~: test
+    [ map gib_aus_Zustand (take 5 (interpretiere_2 pi5 zst1))
+        @?= map gib_aus_Zustand [zst1, zst1, zst1, zst1, zst1]
+    , fst (take 5 (interpretiere_2 pi5 zst1) !! 3) A3 @?= 1
+    , snd (take 4 (interpretiere_2 pi5 zst1) !! 1) L1 @?= True
+    , snd (take 4 (interpretiere_2 pi5 zst1) !! 1) L2 @?= True
+    , snd (take 5 (interpretiere_2 pi5 zst1) !! 2) L5 @?= True
+    ]
 
--}
+
+avb :: Arith_Variablenbelegung
+avb av = 42
+
+lvb :: Log_Variablenbelegung
+lvb lv = True
+
+zst :: Zustand
+zst = (avb, lvb)
+
+testA2 :: Test
+testA2 = "testA2" ~: test
+    [ gib_aus_arith_Varbel avb
+        @?= [(A1, 42), (A2, 42), (A3, 42), (A4, 42), (A5, 42), (A6, 42)]
+    , gib_aus_log_Varbel lvb
+        @?= [ (L1, True)
+            , (L2, True)
+            , (L3, True)
+            , (L4, True)
+            , (L5, True)
+            , (L6, True)
+            ]
+    , gib_aus_Zustand zst
+        @?= ( [(A1, 42), (A2, 42), (A3, 42), (A4, 42), (A5, 42), (A6, 42)]
+            , [ (L1, True)
+              , (L2, True)
+              , (L3, True)
+              , (L4, True)
+              , (L5, True)
+              , (L6, True)
+              ]
+            )
+    , gib_aus_Zustand (interpretiere_1 pi2 zst1)
+        @?= ( [(A1, 1), (A2, 1), (A3, 42), (A4, 1), (A5, 1), (A6, 1)]
+            , [ (L1, True)
+              , (L2, True)
+              , (L3, True)
+              , (L4, True)
+              , (L5, True)
+              , (L6, True)
+              ]
+            )
+    ]
+
+testA3ggt :: Test
+testA3ggt = "testA3ggt" ~: test
+    [ fst
+              (interpretiere_1
+                  ggt
+                  ( \a ->
+                      if a == A1 then x else (if a == A2 then y else undefined)
+                  , const True
+                  )
+              )
+              A3
+          @?= gcd x y
+    | x <- [1 .. 20]
+    , y <- [1 .. 20]
+    ]
+
+
+testA3Fibo :: Test
+testA3Fibo = "testA3Fibo" ~: test
+    [ a6l1 (interpretiere_1 fibo (init n)) @?= (fibo' n, n >= 0)
+    | n <- [-20 .. 20]
+    ]
+  where
+    a6l1 (a, l) = (a A6, l L1)
+    init n = (\a -> if a == A1 then n else undefined, const undefined)
+    fibo' n | n >= 0    = fib n
+            | otherwise = -fib n
+
+
+testA3c :: Test
+testA3c = "testA3c" ~: test
+    [ gib_aus_Zustand azst1
+        @?= ( [(A1, 24), (A2, 60), (A3, 0), (A4, 0), (A5, 0), (A6, 0)]
+            , [ (L1, True)
+              , (L2, True)
+              , (L3, True)
+              , (L4, True)
+              , (L5, True)
+              , (L6, True)
+              ]
+            )
+    , gib_aus_Zustand azst2
+        @?= ( [(A1, 18), (A2, 45), (A3, 3), (A4, 4), (A5, 5), (A6, 6)]
+            , [ (L1, False)
+              , (L2, True)
+              , (L3, False)
+              , (L4, True)
+              , (L5, False)
+              , (L6, True)
+              ]
+            )
+    , gib_aus_Zustand (generiere [] [])
+        @?= ( [(A1, 0), (A2, 0), (A3, 0), (A4, 0), (A5, 0), (A6, 0)]
+            , [ (L1, False)
+              , (L2, False)
+              , (L3, False)
+              , (L4, False)
+              , (L5, False)
+              , (L6, False)
+              ]
+            )
+    , gib_aus_Zustand (generiere [4, 5] [True, True])
+        @?= ( [(A1, 4), (A2, 5), (A3, 0), (A4, 0), (A5, 0), (A6, 0)]
+            , [ (L1, True)
+              , (L2, True)
+              , (L3, False)
+              , (L4, False)
+              , (L5, False)
+              , (L6, False)
+              ]
+            )
+    ]
